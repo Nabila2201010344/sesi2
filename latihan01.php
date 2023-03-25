89eaf22e0c432590e706634dc3fca2b4091dcf9e
Nabila2201010344 <127836877+Nabila2201010344@users.noreply.github.com>
2023-03-25 08:29:30 +0700

    pertemuan 2

---
+++ latihan01.php
@@ -0,0 +1,20 @@
+<?php
+  $judulpage ="latihan 1 PHP";
+  $konten ='<h3>latihan 1 PHP</h3><P>LATIHAN PRATIKUM PEMGROGRAMAN WEB BENTUK DASAR PHP</P>';
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$judulpage;?></title>
+</head>
+<body>
+    
+<?php
+    echo $konten;
+?>
+
+
+</body>
+</html>
